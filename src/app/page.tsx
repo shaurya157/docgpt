21ec879e021a144e1afbd799c7e1076da1d14c77
Redux <shaurya157@gmail.com>
2025-02-01 12:17:45 -0800

    Fixing login redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 'use client';
 
+import { redirect } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
 export default function Landing() {
+  const { data: session } = useSession();
+
+  if (session?.user) {
+    return redirect('/home');
+  }
+
   return <div className="flex h-screen flex-col"></div>;
 }
--- src/components/ChatContent.tsx
+++ src/components/ChatContent.tsx
@@ -340,7 +340,6 @@ const ChatContent = ({
     };
   };
 
-  console.log(activeUserDocument);
   return (
     <motion.div
       className={
