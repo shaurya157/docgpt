206178a605c9968e81c99dfa2b899380593f9897
Redux <shaurya157@gmail.com>
2025-02-05 12:41:24 -0800

    one liner to fix warning

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -49,7 +49,7 @@ const HomeHeader = ({
             value=""
             className="peer sr-only"
             checked={editorOpen}
-            onClick={toggleEditor}
+            onChange={toggleEditor}
           />
           <div className="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-0.5 after:size-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-black peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-black dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-black dark:peer-focus:ring-red-800 rtl:peer-checked:after:-translate-x-full"></div>
         </label>
