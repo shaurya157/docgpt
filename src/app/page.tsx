eb2919c68622aedd9601addd3e8014325581339c
Redux <shaurya157@gmail.com>
2025-04-01 16:55:04 -0700

    linter errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react';
 
-import { ArrowRight } from "lucide-react";
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
